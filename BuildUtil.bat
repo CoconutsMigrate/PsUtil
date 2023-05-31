0bbb36dd54bbdd570acddfc5332f2685f32e04e2
Greg <gregudvari@gmail.com>
2023-05-31 11:22:41 +0200

    Update BuildUtil.bat

--- BuildUtil.bat
+++ BuildUtil.bat
@@ -1,1 +1,4 @@
-@powershell.exe -Command C:\dev\misc\sendTo\BuildUtil.ps1 "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+@echo off
+SET mypath=%~dp0
+echo %mypath:~0,-1%
+powershell.exe -Command %mypath:~0,-1%\BuildUtil.ps1 "%1 %2 %3 %4 %5 %6 %7 %8 %9"
