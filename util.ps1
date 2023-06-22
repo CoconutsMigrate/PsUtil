7c8218532ab3e2bdc964bda17c1652c36889ff69
Greg <gregudvari@gmail.com>
2023-06-22 11:25:23 +0200

    Update util.ps1

--- util.ps1
+++ util.ps1
@@ -12,12 +12,12 @@ function banner( $label ) {
 	Write-Host
 }
 
-function askYesNo( [string] $label, [switch] $defaultNo=$false ) {
+function askYesNo( [string] $label, [string] $default ) {
 	banner -label $label
 	$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
 	$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
 	$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-	$defVal = if ($defaultNo) { 1 } else { 0 }
+	$defVal = if (('false', 'f', '0', 'no', 'n').contains($default.toLower())) { 1 } else { 0 }
 	return (($host.ui.PromptForChoice("", "", $options, $defVal)) -eq 0)
 }
 
